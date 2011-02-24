260aede75ade4b1d77a50449ffda65da7791bda1
Andrew Gustafson <gus4000@gmail.com>
2011-02-24 00:31:01 +0000

    Adding system test comparing simple text and excel files

--- datasetdiff-system-tests/src/test/scala/org/datasetdiff/DatasetComparisonEngineSpecification.scala
+++ datasetdiff-system-tests/src/test/scala/org/datasetdiff/DatasetComparisonEngineSpecification.scala
@@ -4,6 +4,7 @@ import org.specs.Specification
 import org.specs.runner.JUnit4
 import java.text.SimpleDateFormat
 import java.util.Date
+import org.apache.poi.hssf.usermodel.HSSFCell
 
 class DatasetComparisonEngineSpecification extends JUnit4(DatasetComparisonEngineSpecification)
 
@@ -20,10 +21,6 @@ object DatasetComparisonEngineSpecification extends Specification {
       val textFile1 = new TabDelimitedInputDataset(classLoader.getResourceAsStream(fileName1))
       val textFile2 = new TabDelimitedInputDataset(classLoader.getResourceAsStream(fileName2))
 
-      val columnTypes: Map[Int, ClassManifest[_]] = Map(
-        1 -> classManifest[String],
-        2 -> classManifest[BigDecimal]
-      )
       val stringConverter: (String) => String = TextColumnConverters.StringConverter
       val numberConverter: (String) => BigDecimal = TextColumnConverters.NumberConverter
 
@@ -50,10 +47,49 @@ object DatasetComparisonEngineSpecification extends Specification {
       val comparisons: List[Array[ComparisonResult]] = engine.compareDatasets(textFile1, textFile2)
       comparisons.length mustBe 3
       for (rowComparison <- comparisons; cellComparison <- rowComparison) {
-        println(cellComparison)
-        cellComparison.isMatched mustBe true
+        cellComparison must(haveClass[MatchedComparisonResult])
+      }
+    }
+
+    "compare a simple text and excel file successfully" in {
+      val classLoader: ClassLoader = this.getClass.getClassLoader
+
+      val textFileName = "simple1.txt"
+      val excelFileName = "simple1.xls"
+      val textFile = new TabDelimitedInputDataset(classLoader.getResourceAsStream(textFileName))
+      val excelFile = ExcelInputDataset(classLoader.getResourceAsStream(excelFileName), 0)
+
+      val stringTextConverter: (String) => String = TextColumnConverters.StringConverter
+      val numberTextConverter: (String) => BigDecimal = TextColumnConverters.NumberConverter
+      val stringExcelConverter: (HSSFCell) => String = ExcelColumnConverter.String
+      val numberExcelConverter: (HSSFCell) => BigDecimal = ExcelColumnConverter.Number
+
+      val stringColumnComparator = ConvertingColumnComparator[String, String, HSSFCell](stringTextConverter, stringExcelConverter)
+      val numberColumnComparator = ConvertingColumnComparator[BigDecimal, String, HSSFCell](numberTextConverter, numberExcelConverter)
+
+      val dateValueComparator = (left: Date, right: Date) => {
+          val dateFormat = new SimpleDateFormat("yyyyMMdd")
+          dateFormat.format(left) == dateFormat.format(right)
+      }
+      val dateFormat: SimpleDateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm")
+      val dateColumnComparator   = ConvertingColumnComparator[Date, String, HSSFCell](
+        TextColumnConverters.DateConverter(dateFormat),
+        ExcelColumnConverter.Date,
+        dateValueComparator)
+
+      val columnComparators = Map(
+        0 -> stringColumnComparator,
+        1 -> numberColumnComparator,
+        2 -> dateColumnComparator
+      )
+
+      val engine = new DatasetComparisonEngine[String,HSSFCell](columnComparators)
+
+      val comparisons: List[Array[ComparisonResult]] = engine.compareDatasets(textFile, excelFile)
+      comparisons.length mustBe 3
+      for (rowComparison <- comparisons; cellComparison <- rowComparison) {
+        cellComparison must(haveClass[MatchedComparisonResult])
       }
     }
   }
-  
 }
